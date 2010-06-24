1751e57d78d875fd85e38b9482c2d44d297c7cd3
doggie52 <doggie52@f9a2c307-4e71-6544-31ba-9da59db39933>
2010-06-24 14:11:02 +0000

    Added two separate functions to query Google Search results, replacing the old !google command that would simply return a URL. google_search_html() has been fully tested whilst google_search_api() has not - the latter requires the cURL extension which my test-server currently lacks. Both functions will be in use and a check for cURL will be implemented later on. debug_message()'s line is now stored in a variable before outputted, facilitating future modification. The users.inc file's path is now stored in a constant for easy, project-wide editing. The bot will now automatically join a number of channels (specified in the BOT_CHANNELS constant) upon connecting. To allow the above feature to work, I have implemented a greeting-check that checks if the bot is fully connected (i.e. it has been greeted by the server). Slight alteration of debug message sent when the bot quits. debug_message() now appends linebreaks to the end of the line rather than inserting them in front. This prevents certain annoyances in the PHP CLI. config.php has been more elaborately commented to help first-time users set the bot up properly.

--- trunk/config.php
+++ trunk/config.php
@@ -6,24 +6,34 @@
 	 * @email: doggie52@gmail.com
 	 * @url: www.douglasstridsberg.com
 	 */
-
-	// Server configuration
-	define("SERVER_IP", "irc.emulationnetwork.com");
-	define("SERVER_PORT", '6667');
+	 
+	/**
+	 * Server configuration
+	 */
+	define("SERVER_IP", 		"irc.emulationnetwork.com");	// The IP of the server the bot will connect to
+	define("SERVER_PORT", 		'6667');						// The port of the above server
 	
-	// Bot configuration
-	define("BOT_NICKNAME", "Quocom");
-	define("BOT_NAME", "Douglas's Bot");
-	define("BOT_PASSWORD", "pass");
-	define("BOT_QUITMSG", "i am real pro");
+	/**
+	 * Bot configuration
+	 */
+	define("BOT_NICKNAME", 		"Quocom");						// The nickname the bot should use. No spaces allowed
+	define("BOT_NAME", 			"Douglas's Bot");				// The "real" name of the bot
+	define("BOT_PASSWORD", 		"pass");						// The password the bot should use when connecting to the server
+	define("BOT_QUITMSG", 		"i am real pro");				// The message the bot will transmit when disconnecting from the server
+	define("BOT_CHANNELS", 		"#doggie52 #test");				// Array of channels to join on connect. Values must be separated by a space
 	
-	// Debug and log configuration - whatever is shown is logged as well
-	define("GUI", FALSE); 					// Are you using a browser or not?
-	define("DEBUG", TRUE);					// Do you wish to receive debug messages? If not, no logs will be written either
-	define("DEBUG_OUTPUT", FALSE);			// Do you wish to receive the full server output? This will not be logged
-	define("LOG_PATH", "log_%date%.txt");	// Path to the log-file relative to index.php. %date% results in "20100623"
+	/**
+	 * Output configuration - whatever is shown will be logged as well
+	 */
+	define("GUI", 				FALSE); 						// Are you using a browser or not?
+	define("DEBUG", 			TRUE);							// Do you wish to receive debug messages? If not, no logs will be written either
+	define("DEBUG_OUTPUT", 		FALSE);							// Do you wish to receive the full server output? This will not be logged
+	define("LOG_PATH", 			"log_%date%.txt");				// Path to the log-file relative to index.php. %date% results in "20100623"
 	
-	// Misc paths
-	define("DEFINITION_PATH", "encyclopedia.inc");  // The path to the definitions the bot uses relative to index.php.
+	/**
+	 * Miscellaneous paths
+	 */
+	define("DEFINITION_PATH", 	"encyclopedia.inc");			// The path to the definitions the bot uses relative to index.php
+	define("USERS_PATH", 		"users.inc");					// The path to the definitions the bot uses relative to index.php
 
 ?>
