22d1b25a4785e9bf618f171fba0326b0b57a5c05
doggie52@gmail.com <doggie52@gmail.com@f9a2c307-4e71-6544-31ba-9da59db39933>
2011-05-12 15:22:23 +0000

    BRANCH: Bot can now parse basic CLI input without any hickups.

--- branches/interactive-cli/cfg/config.php
+++ branches/interactive-cli/cfg/config.php
@@ -12,7 +12,7 @@
 	/**
 	 * Server configuration
 	 */
-	define("SERVER_IP", 		"irc.emulationnetwork.com");	// The IP of the server the bot will connect to
+	define("SERVER_IP", 		"irc.servercentral.net");	// The IP of the server the bot will connect to
 	define("SERVER_PORT", 		'6667');						// The port of the above server
 	
 	/**
@@ -22,14 +22,14 @@
 	define("BOT_NAME", 			"Douglas's Bot");				// The "real" name of the bot
 	define("BOT_PASSWORD", 		"pass");						// The password the bot should use when connecting to the server
 	define("BOT_QUITMSG", 		"i am real pro");				// The message the bot will transmit when disconnecting from the server
-	define("BOT_CHANNELS", 		"#doggie52 #test");				// Array of channels to join on connect. Values must be separated by a space
+	define("BOT_CHANNELS", 		"#doggie52");				// Array of channels to join on connect. Values must be separated by a space
 	
 	/**
 	 * Output configuration - whatever is shown will be logged as well
 	 */
 	define("GUI", 				FALSE); 						// Are you using a browser or not?
 	define("DEBUG", 			TRUE);							// Do you wish to receive debug messages? If not, no logs will be written either
-	define("DEBUG_OUTPUT", 		FALSE);							// Do you wish to receive the full server output? This will not be logged
+	define("DEBUG_OUTPUT", 		TRUE);							// Do you wish to receive the full server output? This will not be logged
 	define("LOG_PATH", 			"log_%date%.txt");				// Path to the log-file relative to index.php. %date% results in "20100623"
 	define("LOG_APPEND",		FALSE);							// Either append or overwrite the log
 	define("SUPPRESS_PING",		TRUE);							// Suppress the PING/PONG messages from appearing in the log
