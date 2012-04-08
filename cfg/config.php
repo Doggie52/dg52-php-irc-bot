4c4927b1e77234b7fe23ea7d31b1f4b7917ffb9d
doggie52@gmail.com <doggie52@gmail.com@f9a2c307-4e71-6544-31ba-9da59db39933>
2012-04-08 17:19:12 +0000

    Changed structure of config.php, as well as removed redundant constant.

--- cfg/config.php
+++ cfg/config.php
@@ -19,9 +19,9 @@
 	 * Bot configuration
 	 */
 	define("BOT_NICKNAME", 		"Quocom");						// The nickname the bot should use. No spaces allowed
-	define("BOT_NAME", 			"Douglas's Bot");				// The "real" name of the bot
-	define("BOT_PASSWORD", 		"pass");						// The password the bot should use when connecting to the server
-	define("BOT_QUITMSG", 		"i am real pro");				// The message the bot will transmit when disconnecting from the server
+	define("BOT_NAME", 			"dG52 PHP IRC Bot");			// The "real" name of the bot
+	define("BOT_PASSWORD", 		"");							// The password the bot should use when connecting to the server
+	define("BOT_QUITMSG", 		"dG52 PHP IRC Bot");			// The message the bot will transmit when disconnecting from the server
 	define("BOT_CHANNELS", 		"#doggie52");					// Array of channels to join on connect. Values must be separated by a space
 	define("COMMAND_PREFIX",	"!");							// The prefix that identifies a command
 
@@ -33,17 +33,16 @@
 	/**
 	 * Output configuration - whatever is shown will be logged as well
 	 */
-	define("GUI", 				FALSE); 						// Are you using a browser or not?
-	define("DEBUG", 			TRUE);							// Do you wish to receive debug messages? If not, no logs will be written either
-	define("DEBUG_OUTPUT", 		FALSE);							// Do you wish to receive the full server output?
+	define("GUI", 				false); 						// Are you using a browser or not?
+	define("DEBUG", 			true);							// Do you wish to receive debug messages? If not, no logs will be written either
+	define("DEBUG_OUTPUT", 		false);							// Do you wish to receive the full server output?
 	define("LOG_PATH", 			"log_%date%.txt");				// Path to the log-file relative to index.php. %date% results in "20100623"
-	define("LOG_APPEND",		FALSE);							// Either append or overwrite the log
-	define("SUPPRESS_PING",		TRUE);							// Suppress the PING/PONG messages from appearing in the log
+	define("LOG_APPEND",		false);							// Either append or overwrite the log
+	define("SUPPRESS_PING",		true);							// Suppress the PING/PONG messages from appearing in the log
 	
 	/**
 	 * Miscellaneous paths
 	 */
 	define("USERS_PATH", 		"cfg/users.inc");				// The path to the list of administrators the bot uses (relative to index.php)
-	define("DEFINITION_PATH", 	"extra/encyclopedia.inc");		// The path to the definitions the bot uses (relative to index.php)
 
 ?>
